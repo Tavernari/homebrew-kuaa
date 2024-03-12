7d9f5ef9ee6d1f9e4fd0c2c948d2050495597769
Victor C Tavernari <victortavernari@gmail.com>
2024-03-12 00:59:46 +0000

    Update download link

--- Casks/kuaa.rb
+++ Casks/kuaa.rb
@@ -1,8 +1,8 @@
 cask "kuaa" do
     version "3-main"
-    sha256 "3f0191012238ad8a4131543848e3ed8afdfdf25df1ab4741136ed625ffb99324"
+    sha256 "615774e63eff894ce0c24004d3a38b43f251b2cbcfb463a971b967fd5f221363"
   
-    url "https://github.com/Tavernari/kuaa/archive/refs/tags/#{version}.tar.gz"
+    url "https://github.com/Tavernari/kuaa/releases/download/#{version}/kuaa"
     name "Kuaa"
     desc "CLI tool to enhance Git workflow productivity using AI to craft commits."
     homepage "https://kuaa.tools/"
