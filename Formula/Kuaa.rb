fa1bf3e1048218b4bfeba31222f74a2a7b947a3b
Victor C Tavernari <victortavernari@gmail.com>
2024-03-12 11:26:57 +0000

    Update Kuaa formula to version 0.0.1
    - Updated the sha256 checksum for the new release

--- Formula/Kuaa.rb
+++ Formula/Kuaa.rb
@@ -2,7 +2,7 @@ class Kuaa < Formula
     desc "CLI tool to enhance Git workflow productivity using AI to craft commits."
     homepage "https://kuaa.tools"
     url "https://github.com/Tavernari/kuaa/archive/refs/tags/0.0.1.tar.gz"
-    sha256 "86b1cc44cfa93341da90ae63dfc69950d75adbc113cfa795f0d621c970c35cf7"
+    sha256 "befba2a438e9f058138d81c1831115abc1f78db04af113107e51f9d14067bfa9"
   
     depends_on "rust" => :build
   
