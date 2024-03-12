7fdb8bb7d2bffe04437e30629a82b71643fa1e6a
Victor C Tavernari <victortavernari@gmail.com>
2024-03-12 14:21:43 +0000

    diff Update Kuaa formula SHA256 checksum
    The SHA256 checksum for the Kuaa formula has been updated to reflect the new version of the software. This ensures the integrity and authenticity of the downloaded software package.

--- Formula/Kuaa.rb
+++ Formula/Kuaa.rb
@@ -2,7 +2,7 @@ class Kuaa < Formula
     desc "CLI tool to enhance Git workflow productivity using AI to craft commits."
     homepage "https://kuaa.tools"
     url "https://github.com/Tavernari/kuaa/archive/refs/tags/0.0.1.tar.gz"
-    sha256 "befba2a438e9f058138d81c1831115abc1f78db04af113107e51f9d14067bfa9"
+    sha256 "fdb9865ae323d7460fd2caec3d489d399f479fe3eaf86f8cc8f2cb950276f463"
   
     depends_on "rust" => :build
   
